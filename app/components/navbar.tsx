7ba0eb3a802d3d3cee34d2af9766c029a49269db
Maksim Vlasiuk <maksym.lang@gmail.com>
2026-01-31 18:34:53 +0100

    fix theme toggle

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,17 +6,15 @@ import { ToggleTheme } from "@/app/components/toggle-theme";
 export const Navbar = () => {
     return (
         <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="container flex h-14 max-w-screen-2xl items-center justify-between px-4">
+            <div className="flex h-14 items-center justify-between px-4">
                 <div className="mr-4 flex">
                     <Link href="https://github.com/MaksymLeng" className="mr-6 flex items-center space-x-2 font-bold">
                         <span>Landing Preview</span>
                     </Link>
                 </div>
 
-                <div className="flex flex-1 items-center justify-end space-x-2">
-                    <nav className="flex items-end">
-                        <ToggleTheme />
-                    </nav>
+                <div className="flex ml-4">
+                    <ToggleTheme />
                 </div>
             </div>
         </header>
